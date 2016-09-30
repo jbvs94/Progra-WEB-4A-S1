e4f2fabf0677a4eef5c6fcad7968895348aa4668
user <user@debian>
2016-09-30 19:22:01 +0200

    autres projet

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,20 +3,27 @@ class Beer < ActiveRecord::Base
 	has_many :notes
 	has_many :beer_geeks, :through => :notes 
 	accepts_nested_attributes_for :notes, allow_destroy: true
+	validates_associated :notes
+	validates :name ,presence: true
+	validates :description, presence: true
+	#validates :description, inclusion:
+
 
   def moyenne
 	moye=0
         if notes.nil?
-		return
-	else
-		notes.each do |n|
-	      		moye=moye+n.star
+			return
+		else
+			notes.each do |n|
+	      			moye=moye+n.star
        	        end
-			moye=moye/notes.count
+				moye=moye/notes.count unless notes.empty?
+				
+			end
 	end
 	#notes.map { |note| note.star }.sum / notes.count unless notes.empty?
   end
 
 	
 
-end
+
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,22 @@
 class Note < ActiveRecord::Base
   belongs_to :beer_geek
   belongs_to :beer
+  validates :beer_geek, presence: true
+  validates :beer, presence: true
+  validate :limite_sup
+  validate :limite_inf
+  #validate :num
+  #validates :star, numericality: true
+
+  def limite_sup
+  	errors.add(:star, " sup a 10") unless star<=10
+  end
+
+  def limite_inf
+  	errors.add(:star, " inf a 0") unless star>=0  		
+  end
+
+  #def num
+   # errors.add(:star, " il faut un nombre") unless 
+
 end
